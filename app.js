44828428fb06e8c2bb548bd1ccc89f7b73904aff
zhangyunpeng <zhangyunpeng@sensorsdata.cn>
2024-01-08 14:33:17 +0800

    api test

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const Koa = require('koa');
-const app = new Koa();
+const static = require('koa-static');
+const path = require('path');
+const Router = require('@koa/router');
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello World';
+const router = new Router({
+  prefix: '/api',
 });
 
+const app = new Koa();
+
+const staticPath = './public';
+
+// 注册中间件
+app.use(static(path.join(__dirname, staticPath)));
+
+router.get('/', (ctx, next) => {
+  ctx.body = {
+    text: 'hello world!'
+  };
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
 app.listen(3008, () => {
-  console.log('3008项目启动')
+  console.log('3008项目启动');
 });
