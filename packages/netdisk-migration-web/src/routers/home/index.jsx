a58ad0c6d38f36655da943edf5f0e0e360762b71
zhangyunpeng <zhangyunpeng@sensorsdata.cn>
2024-01-08 20:03:22 +0800

    feat: redirect

--- packages/netdisk-migration-web/src/routers/home/index.jsx
+++ packages/netdisk-migration-web/src/routers/home/index.jsx
@@ -2,11 +2,12 @@
  * @Author: zhangyunpeng@sensorsdata.cn
  * @Description: 
  * @Date: 2024-01-08 17:01:28
- * @LastEditTime: 2024-01-08 18:02:59
+ * @LastEditTime: 2024-01-08 20:02:52
  */
 
 import { Button } from 'antd';
 import qs from 'query-string';
+import { useEffect } from 'react';
 
 /**
  * 打开窗口。默认垂直居中
@@ -19,16 +20,33 @@ const openWindow = (url, width = 800, height = 600) => {
   const left = (window.screen.width - 10 - width) / 2; //获得窗口的水平位置;
   const windowFeatures = `left=${left},top=${top},width=${width},height=${height}`;
 
-  window.open(url, '_blank', windowFeatures);
+  return window.open(url, '_blank', windowFeatures);
 }
 
 const Home = () => {
+
+  useEffect(() => {
+    const listenMessage = (e) => {
+      const { data } = e;
+      if (data) {
+        console.log(e, data);
+      } else {
+        console.log(e);
+      }
+    };
+    // 监听postMessage事件
+    window.addEventListener("message", listenMessage)
+    return () => {
+      window.removeEventListener("message", listenMessage);
+    }, []
+  })
+
   const handleLogin = () => {
     // doc: https://pan.baidu.com/union/doc/al0rwqzzl
     const params = {
       response_type: 'code',
       client_id: 'ncYkdVSxHyXRRwuZDnNlVPuD3CVxybNV',
-      redirect_uri: 'https://claude-gitee.gitee.io/',
+      redirect_uri: 'http://pan.claude-hub.cn/redirect.html',
       scope: 'basic,netdisk', // 用户实际授予的权限列表
       confirm_login: 1, // 百度用户已处于登陆状态，会提示是否使用已当前登陆用户对应用授权。
       qrcode: 1 // 登录授权页面将增加扫码登录入口
